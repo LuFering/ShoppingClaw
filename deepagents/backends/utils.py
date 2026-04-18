4aed8c82da5137e97f67a2e4fc8dbc63b2520298
LuFering <2515310244@qq.com>
2026-04-18 13:03:31 +0800

    Delete deepagents directory

--- deepagents/backends/utils.py
+++
@@ -1,4 +0,0 @@
-def validate_path(path:str)->str:
-    pass
-def truncate_if_tool_long(paths:list[str]|str)->str:
-    pass
--- deepagents/middlleware/patch_tool_calls.py
+++
@@ -1,5 +0,0 @@
-from langchain.agents.middleware import AgentMiddleware
-
-
-class PatchToolCallsMiddleware(AgentMiddleware):
-    pass
